d0e9623bedbfad05165f16882e825ac7864e8cec
Robbie K <rkilduff88@gmail.com>
2020-11-20 20:00:24 -0500

    Toggle works

--- public/app.js
+++ public/app.js
@@ -1,10 +1,15 @@
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
 
+    // Toggle
     burger.addEventListener('click', () => {
-        nav.classList.toggle('nav-actice');
+        nav.classList.toggle('nav-active');
     });
+    // Animate Links
 }
 
 navSlide();
+
+
