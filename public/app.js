7ebead27ec41fb6a5e9237d24730ca4f4a4c7f7d
Robbie K <rkilduff88@gmail.com>
2020-11-20 20:53:45 -0500

    X works

--- public/app.js
+++ public/app.js
@@ -2,14 +2,24 @@ const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li');
-
-    // Toggle
+    // Toggle Nav
     burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
-    });
+    
     // Animate Links
+    navLinks.forEach((link, index) => {
+        if (link.style.animation) {
+            link.style.animation = '';
+        } else {
+
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.5}s`;
+            // console.log(index / 5)
+        }
+
+    });
+    // Burger Animation
+    burger.classList.toggle('x');
+});
 }
 
 navSlide();
-
-
